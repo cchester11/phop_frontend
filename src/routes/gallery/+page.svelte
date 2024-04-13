ead6a9a8f18dbe507520724270160db33d4118d3
Charles <cchester1@angelo.edu>
2024-04-13 01:33:45 -0500

    ..

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,1 +1,13 @@
+<script>
+      // on mount fetch metadata containing images
+
+      // store metadata names in an array
+
+      // map the array; for every name, fetch the associated image
+
+      // once image is fetched, store it in an array or object (which ever is better)
+</script>
+
 <h1>Welcome to the gallery</h1>
+
+display the images in a for each manner
