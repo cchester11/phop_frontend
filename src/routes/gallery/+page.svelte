6a204030d2aa7162f6e34bde6002a74f82205dd8
Charles <cchester1@angelo.edu>
2024-04-14 11:41:38 -0500

    images not displaying

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,40 +1,56 @@
 <script>
 	import { onMount } from 'svelte';
-	let imageName = '';
-	const fetchImageNames = 'http://localhost:8000/api/gallery';
-	const fetchPerImage = 'http://localhost:8000/api/sendImage/' + imageName;
-	let imageData = [];
 
-	const fetchImageMetaData = async () => {
+	let imageName = '';
+	let imageData = [];
+	let images = [];
+
+	const fetchImageNames = 'http://localhost:8000/api/gallery';
+	const fetchPerImage = 'http://localhost:8000/api/sendImage/';
+
+	const fetchImages = async () => {
 		try {
 			const response = await fetch(fetchImageNames);
 			const data = await response.json();
 
 			// store metadata names in an array
-			for(let i = 0; i < data.length; i ++) {
-				let curr = data[i]
-				
+			for (let i = 0; i < data.length; i++) {
+				let curr = data[i];
 				const { id, name, imageUrl } = curr;
-
 				imageData.push(name);
-			};
+			}
+
+			// for loop to fetch image according to image name in imageData array
+			for (let i = 0; i < imageData.length; i++) {
+				imageName = imageData[i];
+
+				const response = await fetch(fetchPerImage + imageName);
+				// convert image response into blob
+				const blob = await response.blob();
+				// store images in imageURL's and save to an array
+				const imageUrl = URL.createObjectURL(blob);
+
+				images.push(imageUrl);
+
+				console.log(images)
+			}
 		} catch (error) {
 			console.error('Error fetching images: ', error);
-		};
+		}
 	};
 
 	// on mount fetch metadata containing images
 	onMount(() => {
-		fetchImageMetaData();
+		fetchImages();
 	});
-
-	// map the array; for every name, fetch the associated image
-
-	// once image is fetched, store it in an array or object (which ever is better)
 </script>
 
 <h1>Welcome to the gallery</h1>
 
 <div>
 	<p>display images here</p>
+
+	{#each images as image}
+		<img src={image} alt="description">
+	{/each}
 </div>
