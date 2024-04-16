6c50b8d12cdc6b0a6c223033e094bccad9d70cc0
Charles <cchester1@angelo.edu>
2024-04-16 10:13:08 -0500

    contact page intitial html set up

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,11 @@
 <h1>Welcome to the contact page</h1>
+
+<form method="post" name="contact_form" action="contact-form-handler.php">
+	Your Name:
+	<input type="text" name="name" />
+	Email Address:
+	<input type="text" name="email" />
+	Message:
+	<textarea name="message"></textarea>
+	<input type="submit" value="Submit" />
+</form>
