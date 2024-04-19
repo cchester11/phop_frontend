57215dc367a33878617c2efa042dde87e185bde5
Charles <cchester1@angelo.edu>
2024-04-19 17:34:45 -0400

    labels added to contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,9 +1,15 @@
 <form method="POST" name="contact_form" action="contact-form-handler.php">
-	Your Name:
-	<input type="text" name="name" />
-	Email Address:
-	<input type="text" name="email" />
-	Phone Number:
-	<input type="text" name="phone" />
+	<label>
+		Your Name:
+		<input type="text" name="name" />
+	</label>
+	<label>
+		Email Address:
+		<input type="text" name="email" />
+	</label>
+	<label>
+		Phone Number:
+		<input type="text" name="phone" />
+	</label>
 	<input type="submit" value="Submit" />
 </form>
