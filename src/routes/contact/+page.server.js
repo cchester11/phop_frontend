86129eaad51ed92964ee9dbb5a6816a734f11a64
Charles <cchester1@angelo.edu>
2024-04-20 09:42:39 -0400

    contact form action file working successfully

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -2,24 +2,34 @@
 // this file is used to write "actions" which interface with frontend DOM and API endpoints | actions are stored in key value structure format
 export const actions = {
       default: async ({ request, event }) => {
-            const formData = await request.formData();
-            const email = formData.get('email');
-            const name = formData.get('name');
-            const phone = formData.get('phone');
-            // Process the form data and perform actions
-            const response = await fetch('http://localhost:5173/api/contact', {
-                  method: "POST",
-                  headers: {
-                        "Content-Type": "application/json",
-                        // 'Content-Type': 'application/x-www-form-urlencoded',
-                  },
-                  body: {
-                        name: JSON.stringify(name),
-                        email: JSON.stringify(email),
-                        phone: JSON.stringify(phone)
-                  }
-            })
+            try {
+                  const formData = await request.formData();
+                  const email = formData.get('email');
+                  const name = formData.get('name');
+                  const phone = formData.get('phone');
 
-            return response.json();
+                  // Process the form data and perform actions
+                  const response = await fetch('http://localhost:5173/api/contact', {
+                        method: "POST",
+                        headers: {
+                              "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({
+                              name,
+                              email,
+                              phone
+                        })
+                  });
+
+                  if (!response.ok) {
+                        throw new Error('Failed to send email');
+                  }
+
+                  return await response.json();
+            } catch (error) {
+                  // Handle errors here
+                  console.error('Error sending email:', error);
+                  return { error: 'Error sending email' };
+            }
       }
 };
