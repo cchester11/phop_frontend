584e0747a1ec3d3f5211d242b69c63add953f890
Charles <cchester1@angelo.edu>
2024-04-20 23:49:48 -0400

    styling contact form 2

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,25 +5,31 @@
 <form method="POST" name="contact_form" action="/contact">
 	<div class="flex flex-col">
 		<div class="mt-6">
-			<label for="name">
-				Your Name:
-			</label>
-			<input type="text" name="name" id="name" class="border-2 border-gray-400 rounded"/>
+			<div>
+				<label for="name">Name</label>
+			</div>
+			<div>
+				<input type="text" name="name" id="name" class="border border-gray-400 rounded"/>
+			</div>
 		</div>
 		<div class="mt-6">
-			<label for="email">
-				Email Address:
-			</label>
-			<input type="text" name="email" id="email" class="border-2 border-gray-400 rounded"/>
+			<div>
+				<label for="email">Email</label>
+			</div>
+			<div>
+				<input type="text" name="email" id="email" class="border border-gray-400 rounded"/>
+			</div>
 		</div>
 		<div class="mt-6">
-			<label for="phone">
-				Phone Number:
-			</label>
-			<input type="text" name="phone" id="phone" class="border-2 border-gray-400 rounded"/>
+			<div>
+				<label for="phone">Phone</label>
+			</div>
+			<div>
+				<input type="text" name="phone" id="phone" class="border border-gray-400 rounded"/>
+			</div>
 		</div>
 		<div class="mt-6">
-			<button type="submit" class="rounded-lg bg-purple-600 p-1">Submit</button>
+			<button type="submit" class="rounded text-white bg-purple-600 p-1">Submit</button>
 		</div>
 	</div>
 </form>
