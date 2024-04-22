63f049ff3b56416e3bb0dc27d0b7ca752dda82fc
Charles <cchester1@angelo.edu>
2024-04-22 10:05:07 -0400

    text large h1 layout header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	<nav class="bg-gray-800">
 		<div class="flex justify-between items-center h-20">
 			<!-- Container for the heading, aligned to the left -->
-			<div class="mx-7 text-white">
+			<div class="mx-7 text-indigo-100 text-lg">
 				<h1>Charles Chester Photography</h1>
 			</div>
 
