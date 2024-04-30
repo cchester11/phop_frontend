1e0d0ad77f243337817e9fa88279c55df8139e8b
Charles <cchester1@angelo.edu>
2024-04-30 09:36:04 -0500

    function outline on gallery script

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -8,6 +8,12 @@
 	onMount(async () => {
 		images = await fetchImages();
 	});
+
+	// function here
+	// accepts event
+	// takes event.target and captures the image url associated
+	// saves image url to local storage or prop drills image url the component which displays the full page view of the image
+	// routes user to full page view component where the image url will be displayed
 </script>
 
 <div class="md:masonry-2-col lg:masonry-3-col box-border mx-36 my-20 before:box-inherit after:box-inherit">
