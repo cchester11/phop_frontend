ac460fa4c6e2364455485debc4004038620877d0
Charles <cchester1@angelo.edu>
2024-04-30 11:33:03 -0500

    hover done

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -20,7 +20,7 @@
 		{#each images as image}
 		<div class="break-inside aspect-auto p-1">
 			<img
-				class="object-cover w-auto h-auto rounded-lg hover:cursor-pointer hover:border-2 hover:border-purple-400"
+				class="transition ease-in-out object-cover w-auto h-auto rounded-lg hover:cursor-pointer hover:border-2 hover:border-purple-400 hover:scale-95"
 				src={image}
 				alt="description"
 			/>
