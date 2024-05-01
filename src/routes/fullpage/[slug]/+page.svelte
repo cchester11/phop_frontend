241b8fb6774685811e6e5d23bbd268aafa87f8d4
Charles <cchester1@angelo.edu>
2024-05-01 17:52:14 -0500

    x symbol over the fullpage image

--- src/routes/fullpage/[slug]/+page.svelte
+++ src/routes/fullpage/[slug]/+page.svelte
@@ -5,6 +5,13 @@
 
 <div class="w-full flex justify-center items-center mb-16">
 	<div class="w-1/2">
+		<div class="w-full flex justify-end items-center">
+			<button
+				class="mb-4 p-2 rounded-lg bg-purple-400 text-white text-sm hover:bg-purple-600 focus:outline-none"
+			>
+				<a href="/gallery"> X </a>
+			</button>
+		</div>
 		<img src={data.image} alt="Something" class="rounded" />
 	</div>
 </div>
