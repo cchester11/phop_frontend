2c2c66357865e8441ca7e4b0d0c9a01baebb2798
Charles <cchester1@angelo.edu>
2024-05-09 12:43:00 -0500

    css responsive design 1 | welcome page letters smaller at small screen size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="flex flex-col justify-center">
 	<div class="w-full flex justify-center">
 		<p
-			class="text-4xl text-slate-900"
+			class="text-4xl sm:text-xl text-slate-900"
 			style="font-family: Notable, sans-serif; font-weight: 400; font-style: normal;"
 		>
 			Thanks for checking out my work. Please review the gallery!
